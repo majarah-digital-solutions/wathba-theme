01f4bcd5c7a9dcf813acff5454fa9a73e9cf386f
khalid-walid00 <khalidk50050@gmail.com>
2024-11-27 17:37:22 +0200

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
             DEFAULT: '1rem',
             sm: '2rem',
             lg: '4rem',
-            xl: '5rem',
+            xl: '4.5rem',
             '2xl': '6rem',
         },
       },
