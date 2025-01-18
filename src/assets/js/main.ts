61f979fcc416c297860935b75fa2f080045cd0a1
coder371 <abdalstar371371@gmail.com>
2025-01-18 03:16:11 +0200

    Refactor GlobalState and update script source in layout
    - Updated GlobalState in main.ts to use optional chaining for safer access to cart items and products.
    - Changed script source in layout.njk to a new local address for qumra-gearbox.js, ensuring proper loading in the development environment.

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -18,9 +18,9 @@ interface GlobalStateInterface {
 }
 function GlobalState(): GlobalStateInterface {
     return {
-        itemsCount: window.__qumra__.context.cart.items.length,
-        products: window.__qumra__.context.products,
-        cardItems: window.__qumra__.context.cart as any,
+        itemsCount: window.__qumra__.context.cart?.items?.length,
+        products: window.__qumra__?.context?.products,
+        cardItems: window.__qumra__?.context?.cart as any,
         Modal: {
             open: false,
             type: ""
