d7e72865f2d175d500d5c91d5dce2ee508c1b1c2
FARES MOHAMED <119041871+Faress-MoHamed@users.noreply.github.com>
2025-01-18 15:03:15 +0200

    make closeModal function

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,96 +1,102 @@
-
 interface GlobalStateInterface {
-    itemsCount: number;
-    products: any;
-    cardItems: any;
-    Modal: {
-        open: boolean;
-        type: string
-    };
-    couponLoading: boolean;
-    coupon?: string;
-    addCartItem(product: any, quantity: any, properties: any): void;
-    updatePrductCount(_id: string, quantity: string | number): void;
-    removeProduct(_id: string): void;
-    checkCoupon(code: string): void;
-    toggle(type: string): void;
-    searchProducts(search: any): void
+	itemsCount: number;
+	products: any;
+	cardItems: any;
+	Modal: {
+		open: boolean;
+		type: string;
+	};
+	couponLoading: boolean;
+	coupon?: string;
+	addCartItem(product: any, quantity: any, properties: any): void;
+	updatePrductCount(_id: string, quantity: string | number): void;
+	removeProduct(_id: string): void;
+	checkCoupon(code: string): void;
+	toggle(type: string): void;
+	closeModal(): void;
+	searchProducts(search: any): void;
 }
 function GlobalState(): GlobalStateInterface {
-    return {
-        itemsCount: window.__qumra__.context.cart?.items?.length,
-        products: window.__qumra__?.context?.products,
-        cardItems: window.__qumra__?.context?.cart as any,
-        Modal: {
-            open: false,
-            type: ""
-        },
-        couponLoading: false,
-        addCartItem(product: any, quantity: any, properties: any) {
-            window.Qumra.cart.addCartItem(product, quantity, properties).then((res: any) => (
-                this.cardItems = res,
-                this.itemsCount = res.items.length
-
-            ))
-        },
-        updatePrductCount(_id: string, quantity: string | number) {
-            window.Qumra.cart.updateQuantity(_id, quantity).then((res: any) => (
-                this.cardItems.items.map((item: any) => {
-                    if (item._id === _id) {
-                        item.quantity = quantity
-                    }
-                }),
-                this.cardItems.totalPrice = res.totalPrice,
-                this.cardItems.productDiscount = res.productDiscount,
-                this.cardItems.totalProducts = res.totalProducts,
-                this.cardItems.totalProductsAfterDiscount = res.totalProductsAfterDiscount
-            ))
-        },
-        removeProduct(_id: string) {
-            window.Qumra.cart.clearCartItem(_id).then((res: any) => (
-                this.itemsCount = this.cardItems.items.length - 1,
-                this.cardItems.items = this.cardItems.items.filter((item: any) => item._id !== _id),
-                this.cardItems.totalPrice = res.totalPrice,
-                this.cardItems.productDiscount = res.productDiscount,
-                this.cardItems.totalProducts = res.totalProducts,
-                this.cardItems.totalProductsAfterDiscount = res.totalProductsAfterDiscount
-                // this.cardItems = res
-            ))
-
-        },
-        checkCoupon(code: string) {
-            this.couponLoading = true;
-            window.Qumra.cart.redeemCode(code).finally(() => {
-                this.couponLoading = false;
-            });
-        },
-        toggle(type) {
-
-            if (this.Modal.type === type) {
-                this.Modal.open = !this.Modal.open;
-            } else {
-                this.Modal.type = type;
-                this.Modal.open = true;
-            }
-            if (type === "cart") {
-                this.couponLoading = false
-                if (this.Modal.open) {
-                    window.Qumra.dom.disableScrolling()
-                } else {
-                    window.Qumra.dom.enableScrolling()
-                }
-                this.coupon = ""
-            }
-        },
-        searchProducts(search: any) {
-            window.location.href = `/search/?q=${search}`
-        },
-
-
-
-    }
+	return {
+		itemsCount: window.__qumra__.context.cart?.items?.length,
+		products: window.__qumra__?.context?.products,
+		cardItems: window.__qumra__?.context?.cart as any,
+		Modal: {
+			open: false,
+			type: "",
+		},
+		couponLoading: false,
+		addCartItem(product: any, quantity: any, properties: any) {
+			window.Qumra.cart
+				.addCartItem(product, quantity, properties)
+				.then(
+					(res: any) => (
+						(this.cardItems = res), (this.itemsCount = res.items.length)
+					)
+				);
+		},
+		updatePrductCount(_id: string, quantity: string | number) {
+			window.Qumra.cart.updateQuantity(_id, quantity).then(
+				(res: any) => (
+					this.cardItems.items.map((item: any) => {
+						if (item._id === _id) {
+							item.quantity = quantity;
+						}
+					}),
+					(this.cardItems.totalPrice = res.totalPrice),
+					(this.cardItems.productDiscount = res.productDiscount),
+					(this.cardItems.totalProducts = res.totalProducts),
+					(this.cardItems.totalProductsAfterDiscount =
+						res.totalProductsAfterDiscount)
+				)
+			);
+		},
+		removeProduct(_id: string) {
+			window.Qumra.cart.clearCartItem(_id).then(
+				(res: any) => (
+					(this.itemsCount = this.cardItems.items.length - 1),
+					(this.cardItems.items = this.cardItems.items.filter(
+						(item: any) => item._id !== _id
+					)),
+					(this.cardItems.totalPrice = res.totalPrice),
+					(this.cardItems.productDiscount = res.productDiscount),
+					(this.cardItems.totalProducts = res.totalProducts),
+					(this.cardItems.totalProductsAfterDiscount =
+						res.totalProductsAfterDiscount)
+					// this.cardItems = res
+				)
+			);
+		},
+		checkCoupon(code: string) {
+			this.couponLoading = true;
+			window.Qumra.cart.redeemCode(code).finally(() => {
+				this.couponLoading = false;
+			});
+		},
+		toggle(type) {
+			if (this.Modal.type === type) {
+				this.Modal.open = !this.Modal.open;
+			} else {
+				this.Modal.type = type;
+				this.Modal.open = true;
+			}
+			if (type === "cart") {
+				this.couponLoading = false;
+				if (this.Modal.open) {
+					window.Qumra.dom.disableScrolling();
+				} else {
+					window.Qumra.dom.enableScrolling();
+				}
+				this.coupon = "";
+			}
+		},
+		closeModal() {
+			this.Modal.open = false;
+		},
+		searchProducts(search: any) {
+			window.location.href = `/search/?q=${search}`;
+		},
+	};
 }
-
-
 
 window.GlobalState = GlobalState;
