c082b1026b2e08837f8b1d89f2f52c9fb8a57881
FARES MOHAMED <119041871+Faress-MoHamed@users.noreply.github.com>
2025-01-19 15:09:42 +0200

    fix scrolling bug

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,82 +1,78 @@
 import { CartItem } from "../../types/cart.js";
 
 interface GlobalStateInterface {
-  itemsCount: number;
-  products: any;
-  cart: any;
-  Modal: {
-    open: boolean;
-    type: string;
-  };
-  couponLoading: boolean;
-  coupon?: string;
-  updateCart: (data: any) => void;
-  addCartItem?(product: any, quantity: any, properties: any): void;
-  updatePrductCount?(_id: string, quantity: string | number): void;
-  removeProduct?(_id: string): void;
-  checkCoupon?(code: string): void;
-  toggle?(type: string): void;
-  closeModal(): void;
-  searchProducts(search: any): void;
-  init: () => void;
+	itemsCount: number;
+	products: any;
+	cart: any;
+	Modal: {
+		open: boolean;
+		type: string;
+	};
+	couponLoading: boolean;
+	coupon?: string;
+	updateCart: (data: any) => void;
+	addCartItem?(product: any, quantity: any, properties: any): void;
+	updatePrductCount?(_id: string, quantity: string | number): void;
+	removeProduct?(_id: string): void;
+	checkCoupon?(code: string): void;
+	toggle?(type: string): void;
+	closeModal(): void;
+	searchProducts(search: any): void;
+	init: () => void;
 }
 
 function GlobalState(): GlobalStateInterface {
-  return {
-    itemsCount: window.__qumra__?.context.cart?.items?.length ?? 0,
-    products: window.__qumra__?.context?.products ?? [],
-    cart: window.__qumra__?.context?.cart,
-    Modal: {
-      open: false,
-      type: "",
-    },
-    couponLoading: false,
-    updateCart(data: any) {
-      this.cart = data;
-    },
-    toggle(type) {
-      console.log("Modal", this.Modal, type);
-      if (this.Modal.type === type) {
-        this.Modal.open = !this.Modal.open;
-      } else {
-        this.Modal.type = type;
-        this.Modal.open = true;
-      }
-      if (this.Modal.open) {
-        this.couponLoading = false;
-        if (this.Modal.open) {
-          Qumra.dom.disableScrolling();
-        } else {
-          Qumra.dom.enableScrolling();
-        }
-        this.coupon = "";
-      }
-    },
-    closeModal() {
-      this.Modal.open = false;
-    },
-    searchProducts(search: any) {
-      window.location.href = `/search/?q=${search}`;
-    },
-    init() {
-      window.updateCart = this.updateCart.bind(this);
-    },
-  };
+	return {
+		itemsCount: window.__qumra__?.context.cart?.items?.length ?? 0,
+		products: window.__qumra__?.context?.products ?? [],
+		cart: window.__qumra__?.context?.cart,
+		Modal: {
+			open: false,
+			type: "",
+		},
+		couponLoading: false,
+		updateCart(data: any) {
+			this.cart = data;
+		},
+		toggle(type) {
+			console.log("Modal", this.Modal, type);
+			if (this.Modal.type === type) {
+				this.Modal.open = !this.Modal.open;
+			} else {
+				this.Modal.type = type;
+				this.Modal.open = true;
+			}
+			this.couponLoading = false;
+			if (this.Modal.open) {
+				Qumra.dom.disableScrolling();
+			} else {
+				Qumra.dom.enableScrolling();
+			}
+			this.coupon = "";
+		},
+		closeModal() {
+			this.Modal.open = false;
+		},
+		searchProducts(search: any) {
+			window.location.href = `/search/?q=${search}`;
+		},
+		init() {
+			window.updateCart = this.updateCart.bind(this);
+		},
+	};
 }
 document.addEventListener("DOMContentLoaded", () => {
-  if (window.Qumra && window.Qumra.events) {
-    window.Qumra.events.on(
-      window.Qumra.events.QumraEventName.CartUpdate,
-      (data: any) => {
-        console.log("🚀 ~ document.addEventListener ~ data:", data)
-        window.updateCart(data);
-      }
-    );
-  } else {
-    console.error("Qumra is not defined or events are not available.");
-  }
+	if (window.Qumra && window.Qumra.events) {
+		window.Qumra.events.on(
+			window.Qumra.events.QumraEventName.CartUpdate,
+			(data: any) => {
+				console.log("🚀 ~ document.addEventListener ~ data:", data);
+				window.updateCart(data);
+			}
+		);
+	} else {
+		console.error("Qumra is not defined or events are not available.");
+	}
 });
-
-
 
 window.GlobalState = GlobalState;
